81f6fd76db74cd786533de73490e8aacc873d4e0
JDH-993 <sonya.shlyakhova@mail.ru>
2024-07-10 14:20:58 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+shet = 0
+znach = my_list[shet]
+
+while znach >=0:
+    if znach > 0:
+        print(znach)
+    shet = shet + 1
+    znach = my_list[shet]
